213ec1dd6bfad7ffeb8cb1e02c2856bc8ff29528
byonghwan_son <sonstein@daum.net>
2024-04-09 00:17:03 +0900

    simple java

---
+++ src/variable/Var1.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var1 {
+  public static void main(String[] args) {
+    int[] var1 = new int[5];
+    System.out.println(var1);
+
+    for (int i : var1) {
+      System.out.println(i);
+    }
+  }
+}
